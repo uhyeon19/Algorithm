c546fe40867a783c504e6805caefebcb71752d1a
김수린 <uhyeon1009@gmail.com>
2023-07-26 23:26:10 +0900

    BOJ2075, BOJ14503(미완)

---
+++ DataStruct&Simulation/src/SR/BOJ14503.java
@@ -0,0 +1,21 @@
+package SR;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ14503 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int [][]arr = new int[N][M];
+        for(int i = 0; i < N; i++){
+            for(int k = 0; k < M; k++){
+
+            }
+        }
+
+    }
+}
