e0e60c121d376693d1a3d224e170ee7ea9666929
김수린 <uhyeon1009@gmail.com>
2023-08-01 23:30:18 +0900

    BOJ11729

---
+++ Recursion/src/SR/BOJ11729.java
@@ -0,0 +1,26 @@
+package SR;
+
+import java.io.*;
+
+public class BOJ11729 {
+	static int cnt = 0;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void hanoi(int n, int start, int end, int other) {
+		if(n == 0) return;
+		else {
+			cnt++;
+			hanoi(n - 1, start, other, end);
+			sb.append(start + " " + end + "\n");
+			hanoi(n - 1, other, end, start);
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		hanoi(n, 1, 3, 2);
+		System.out.println(cnt);
+		System.out.println(sb.toString());
+	}
+}
