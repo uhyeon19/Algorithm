e8319d824ab7eee2440613a7fa650f3a76b9a327
김수린 <uhyeon1009@gmail.com>
2023-08-19 16:17:54 +0900

    BOJ1026, BOJ1789, BOJ13305

---
+++ GRID/src/SR/BOJ1026.java
@@ -0,0 +1,27 @@
+package SR;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ1026 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int S, aArr[], bArr[];
+
+    public static void main(String[] args) throws IOException {
+        S = Integer.parseInt(br.readLine());
+        aArr = new int[S];
+        bArr = new int[S];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < S; i++) aArr[i] = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < S; i++) bArr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(aArr);
+        Arrays.sort(bArr);
+        int ans = 0;
+        for(int i = 0; i < S; i++){
+            ans += aArr[i] * bArr[S - i - 1];
+        }
+        System.out.println(ans);
+    }
+}
