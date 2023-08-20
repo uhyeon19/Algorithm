791255daf5bc76f2bda49f3300194e7799f0077e
김수린 <uhyeon1009@gmail.com>
2023-08-20 20:12:02 +0900

    BOJ1021(미완), BOJ2161, BOJ11866

---
+++ DataStruct&Simulation/src/SR/BOJ1021.java
@@ -0,0 +1,15 @@
+package SR;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ1021 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static int N, M, cmd;
+	
+	public static void main(String[] args) throws IOException {
+		
+	}
+}
---
+++ DataStruct&Simulation/src/SR/BOJ11866.java
@@ -0,0 +1,34 @@
+package SR;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ11866 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	static int N, K;
+	static Queue<Integer> queue = new ArrayDeque<>();
+	
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		for(int i = 1; i <= N; i++) {
+			queue.offer(i);
+		}
+		
+		sb.append('<');
+		while(!queue.isEmpty()) {
+			for(int i = 0; i < K - 1; i++) {
+				int tmp = queue.poll();
+				queue.offer(tmp);
+			}
+			sb.append(queue.poll()).append(queue.isEmpty() ? ">" : ", ");
+		}
+		bw.write(sb.toString() + "\n");
+		bw.flush();
+		bw.close();
+	}
+}
