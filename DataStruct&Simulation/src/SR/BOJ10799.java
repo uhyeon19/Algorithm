1422055fa9d40a1869d4f9a2226333946b0362a4
김수린 <uhyeon1009@gmail.com>
2023-08-20 18:53:17 +0900

    BOJ10799, BOJ10828

---
+++ DataStruct&Simulation/src/SR/BOJ10799.java
@@ -0,0 +1,31 @@
+package SR;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ10799 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int ans = 0;
+	static Stack<Character> stack = new Stack<>();
+
+	public static void main(String[] args) throws IOException {
+		String str = br.readLine();
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == '(') {
+				stack.push(str.charAt(i));
+			} else {
+				stack.pop();
+				
+				if(str.charAt(i - 1) == '(') {
+					ans += stack.size();
+				} else {
+					ans++;
+				}
+			}
+		}
+		bw.write(ans + "\n");
+		bw.flush();
+		bw.close();
+	}
+}
